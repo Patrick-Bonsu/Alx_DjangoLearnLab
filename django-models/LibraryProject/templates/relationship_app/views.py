833259ca974c566121acf30e4c865c0e6fa46145
Patrick Yeboah Bonsu <rick@Patricks-MacBook-Pro.local>
2025-09-30 08:00:29 -0400

    fix: changed the folder strucuture of relationship_app aand template;

---
+++ django-models/LibraryProject/templates/relationship_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.detail import DetailView
+from .models import Library, Book
+
+# --- Function-Based View: List all books ---
+def list_books(request):
+    books = Book.objects.all()
+    # ✅ use app-specific template path
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+# --- Class-Based View: Library details ---
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # ✅ app-specific path
+    context_object_name = 'library'
