f404d2782580a9a42e027b36e46568fa7f1b500a
Patrick Yeboah Bonsu <rick@Patricks-MacBook-Pro.local>
2025-09-30 07:17:46 -0400

    feat: add relationship_app with models and query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,7 +10,8 @@ from relationship_app.models import Author, Book, Library
 # 1. Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    # ✅ Using objects.filter instead of reverse relation
+    return Book.objects.filter(author=author)
 
 # 2. List all books in a library
 def books_in_library(library_name):
